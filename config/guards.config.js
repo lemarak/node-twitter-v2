715f9a87abc13b3d5489e47e0ac6f8520cdfc2bc
Stéphane Dauguet <sdauguet@gmail.com>
2023-01-09 16:42:13 +0100

    fix signout

--- config/guards.config.js
+++ config/guards.config.js
@@ -1,9 +1,7 @@
 exports.ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log(" Connecté");
     next();
   } else {
-    console.log("Pas connecté");
     res.redirect("/auth/signin/form");
   }
 };
